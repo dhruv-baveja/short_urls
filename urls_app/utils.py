5c6dd3e5b41a23f691a94448a0dff03979084321
dhruv-baveja <dhruvbaveja1010@gmail.com>
2018-03-24 12:46:50 +0530

    Generate short_url using SHA256 hashing algo

--- urls_app/utils.py
+++ urls_app/utils.py
@@ -1,24 +1,10 @@
-def get_short_url(url_id):
-    if url_id == 0:
-        return '0'
+import hashlib
 
-    string = ""
-    while url_id > 0:
-        remainder = int(url_id % 62)
-        string = true_chr(remainder) + string
-        url_id = int(url_id/62)
-    return string
+import arrow
 
 
-def true_chr(integer):
-    lower_offset = 61
-    upper_offset = 55
-    digit_offset = 48
-    if integer < 10:
-        return chr(integer + digit_offset)
-    elif 10 <= integer <= 35:
-        return chr(integer + upper_offset)
-    elif 36 <= integer < 62:
-        return chr(integer + lower_offset)
-
-
+def get_short_url(long_url):
+    timestamp = arrow.utcnow().datetime
+    str_input = long_url + str(timestamp)
+    hashed_str = hashlib.sha256(str_input.encode('utf-8')).hexdigest()
+    return hashed_str[::8]
